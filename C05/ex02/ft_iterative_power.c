5aba876506dff7692f69a98566e8f260edb30968
Emre <64619512+OsmanEmreYilmaz@users.noreply.github.com>
2022-10-26 15:58:10 +0300

    Add files via upload

---
+++ C05/ex02/ft_iterative_power.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: osyilmaz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/24 12:17:34 by osyilmaz          #+#    #+#             */
+/*   Updated: 2022/02/24 13:10:27 by osyilmaz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_iterative_power(int nb, int power)
+{
+	int	us;
+
+	us = nb;
+	if (power < 0)
+		return (0);
+	else if (power == 0)
+		return (1);
+	while (power > 1)
+	{
+		us *= nb;
+		power--;
+	}
+	return (us);
+}
+/*
+#include <stdio.h>
+
+int ft_iterative_power(int nb, int power);
+
+int		main(void)
+{
+	printf("-3 ^-1 = %d (0)\n", ft_iterative_power(-3, -1));
+	printf("-3 ^ 0 = %d (1)\n", ft_iterative_power(-3, 0));
+	printf("-3 ^ 1 = %d (-3)\n", ft_iterative_power(-3, 1));
+	printf("-3 ^ 2 = %d (9)\n", ft_iterative_power(-3, 2));
+	printf("-3 ^ 3 = %d (-27)\n", ft_iterative_power(-3, 3));
+	printf(" 0 ^-1 = %d (0)\n", ft_iterative_power(0, -1));
+	printf(" 0 ^ 0 = %d (1)\n", ft_iterative_power(0, 0));
+	printf(" 0 ^ 2 = %d (0)\n", ft_iterative_power(0, 2));
+	printf(" 4 ^-1 = %d (0)\n", ft_iterative_power(4, -1));
+	printf(" 4 ^ 0 = %d (1)\n", ft_iterative_power(4, 0));
+	printf(" 4 ^ 1 = %d (4)\n", ft_iterative_power(4, 1));
+	printf(" 4 ^ 2 = %d (16)\n", ft_iterative_power(4, 2));
+	printf(" 4 ^ 3 = %d (64)\n", ft_iterative_power(4, 3));
+	
+}*/
